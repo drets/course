5cd41038b60daf5065e7b387e5cc31c3aa63740f
drets <dmitryrets@gmail.com>
2016-03-03 21:24:04 +0100

    Add List.hs solutions

--- src/Course/List.hs
+++ src/Course/List.hs
@@ -75,8 +75,8 @@ headOr ::
   a
   -> List a
   -> a
-headOr =
-  error "todo: Course.List#headOr"
+headOr _ (x :. _) = x
+headOr y _        = y
 
 -- | The product of the elements of a list.
 --
@@ -88,8 +88,8 @@ headOr =
 product ::
   List Int
   -> Int
-product =
-  error "todo: Course.List#product"
+product Nil       = 1
+product (x :. xs) = x * product xs
 
 -- | Sum the elements of the list.
 --
@@ -103,8 +103,8 @@ product =
 sum ::
   List Int
   -> Int
-sum =
-  error "todo: Course.List#sum"
+sum Nil       = 0
+sum (x :. xs) = x + sum xs
 
 -- | Return the length of the list.
 --
@@ -115,8 +115,8 @@ sum =
 length ::
   List a
   -> Int
-length =
-  error "todo: Course.List#length"
+length Nil       = 0
+length (_ :. xs) = 1 + length xs
 
 -- | Map the given function on each element of the list.
 --
@@ -130,8 +130,8 @@ map ::
   (a -> b)
   -> List a
   -> List b
-map =
-  error "todo: Course.List#map"
+map _ Nil       = Nil
+map f (x :. xs) = f x :. map f xs
 
 -- | Return elements satisfying the given predicate.
 --
@@ -147,8 +147,8 @@ filter ::
   (a -> Bool)
   -> List a
   -> List a
-filter =
-  error "todo: Course.List#filter"
+filter _ Nil       = Nil
+filter f (x :. xs) = if f x then x :. filter f xs else filter f xs
 
 -- | Append two lists to a new list.
 --
@@ -166,8 +166,8 @@ filter =
   List a
   -> List a
   -> List a
-(++) =
-  error "todo: Course.List#(++)"
+(++) Nil ys       = ys
+(++) (x :. xs) ys = x :. xs ++ ys
 
 infixr 5 ++
 
@@ -184,8 +184,8 @@ infixr 5 ++
 flatten ::
   List (List a)
   -> List a
-flatten =
-  error "todo: Course.List#flatten"
+flatten Nil       = Nil
+flatten (x :. xs) = x ++ flatten xs
 
 -- | Map a function then flatten to a list.
 --
@@ -201,8 +201,8 @@ flatMap ::
   (a -> List b)
   -> List a
   -> List b
-flatMap =
-  error "todo: Course.List#flatMap"
+flatMap _ Nil       = Nil
+flatMap f (x :. xs) = f x ++ flatMap f xs
 
 -- | Flatten a list of lists to a list (again).
 -- HOWEVER, this time use the /flatMap/ function that you just wrote.
@@ -211,8 +211,7 @@ flatMap =
 flattenAgain ::
   List (List a)
   -> List a
-flattenAgain =
-  error "todo: Course.List#flattenAgain"
+flattenAgain = flatMap id
 
 -- | Convert a list of optional values to an optional list of values.
 --
@@ -239,8 +238,12 @@ flattenAgain =
 seqOptional ::
   List (Optional a)
   -> Optional (List a)
-seqOptional =
-  error "todo: Course.List#seqOptional"
+seqOptional = seqOptional' Nil
+  where
+    seqOptional' :: List a -> List (Optional a) -> Optional (List a)
+    seqOptional' acc Nil            = Full acc
+    seqOptional' _   (Empty :. _)   = Empty
+    seqOptional' acc (Full x :. xs) = seqOptional' (acc ++ (x :. Nil)) xs
 
 -- | Find the first element in the list matching the predicate.
 --
@@ -262,11 +265,10 @@ find ::
   (a -> Bool)
   -> List a
   -> Optional a
-find =
-  error "todo: Course.List#find"
+find _ Nil    = Empty
+find f (x :. xs) = if f x then Full x else find f xs
 
 -- | Determine if the length of the given list is greater than 4.
---
 -- >>> lengthGT4 (1 :. 3 :. 5 :. Nil)
 -- False
 --
@@ -281,8 +283,7 @@ find =
 lengthGT4 ::
   List a
   -> Bool
-lengthGT4 =
-  error "todo: Course.List#lengthGT4"
+lengthGT4 xs = length xs > 4
 
 -- | Reverse a list.
 --
@@ -298,8 +299,11 @@ lengthGT4 =
 reverse ::
   List a
   -> List a
-reverse =
-  error "todo: Course.List#reverse"
+reverse = reverse' Nil
+  where
+    reverse' :: List a -> List a -> List a
+    reverse' acc Nil       = acc
+    reverse' acc (x :. xs) = reverse' (x :. acc) xs
 
 -- | Produce an infinite `List` that seeds with the given value at its head,
 -- then runs the given function for subsequent elements
@@ -313,8 +317,7 @@ produce ::
   (a -> a)
   -> a
   -> List a
-produce =
-  error "todo: Course.List#produce"
+produce f x = x :. produce f (f x)
 
 -- | Do anything other than reverse a list.
 -- Is it even possible?
@@ -328,9 +331,9 @@ produce =
 notReverse ::
   List a
   -> List a
-notReverse =
-  error "todo: Is it even possible?"
-
+notReverse = reverse
+ -- Is it even possible?
+ -- No
 ---- End of list exercises
 
 largeList ::
