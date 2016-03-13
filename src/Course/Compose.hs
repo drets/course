047a22970d28e26424dc5d828f8263c785069b41
drets <dmitryrets@gmail.com>
2016-03-13 01:59:48 +0100

    Compose Add solutions

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -14,22 +14,15 @@ newtype Compose f g a =
   Compose (f (g a))
 
 -- Implement a Functor instance for Compose
-instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  f <$> Compose x = Compose ((<$>) f <$> x)
 
-instance (Applicative f, Applicative g) =>
-  Applicative (Compose f g) where
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+  pure x = Compose (pure (pure x))
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  Compose f <*> Compose x = Compose ((<*>) <$> f <*> x)
 
-instance (Monad f, Monad g) =>
-  Monad (Compose f g) where
--- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+instance (Monad f, Monad g) => Monad (Compose f g) where
+  (=<<) = undefined
+-- not possible to achieve
